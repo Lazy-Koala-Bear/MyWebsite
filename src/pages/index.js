642904517be0d2e85b7cdac859f84f00e39f902c
Lazy-Koala-Bear <yuexu97@gmail.com>
2019-06-03 13:54:36 +0200

    rebuild

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
 			<Layout>
 				<h1> Hello. </h1>
 				<h2> I'm Penny and I'm a moch </h2>
-				<p> Want to contact me? <Link to="/contact"> contact</Link> </p>
 			</Layout>
 		</div>
 	)
